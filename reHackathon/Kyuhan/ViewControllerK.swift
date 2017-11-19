b0364081f013fce94d0fdbf9e31ee9f4a81a6c6f
spicystar <skh0206@gmail.com>
2017-11-19 13:49:01 +0900

    new file:   reHackathon/Kyuhan/StoryboardK.storyboard 	new file:   reHackathon/Kyuhan/ViewControllerK.swift

---
+++ reHackathon/Kyuhan/ViewControllerK.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerK.swift
+//  reHackathon
+//
+//  Created by Kyuhan Shin on 2017. 11. 19..
+//  Copyright © 2017년 류성두. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerK: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
